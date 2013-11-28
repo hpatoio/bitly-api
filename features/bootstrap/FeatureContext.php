608d5e12129f9f6e0664e83b23af6116ebee0e77
Simone Fumagalli <simone@iliveinperego.com>
2013-11-28 11:54:51 +0100

    Fixed properties name and casting

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -21,6 +21,7 @@ class FeatureContext extends BehatContext
 {
     
     private $request_parameters;
+    private $context_parameters;
     
     /**
      * Initializes context.
@@ -30,6 +31,7 @@ class FeatureContext extends BehatContext
      */
     public function __construct(array $parameters)
     {
+        $this->context_parameters = $parameters;
         $this->request_parameters = array();
         $this->client = new Bitly\Api\BitlyClient("1ade4fb6754a0db893354240f6a1da2bf1dd9901");
     }
@@ -40,9 +42,13 @@ class FeatureContext extends BehatContext
     public function pushParams(TableNode $parametersTable)
     {
         foreach ($parametersTable->getHash() as $parameterHash) {
-            if ($parameterHash['casting'])
-                settype($parameterHash['value'], $parameterHash['casting']);
-            $this->parameters[$parameterHash['name']] = $parameterHash['value'];
+            if ($parameterHash['casting']) {
+                if ($parameterHash['casting'] == 'Array')
+                    $parameterHash['value'] = explode(",",$parameterHash['value']);
+                else
+                    settype($parameterHash['value'], $parameterHash['casting']);
+            }
+            $this->request_parameters[$parameterHash['name']] = $parameterHash['value'];
         }
        
     }
@@ -52,7 +58,8 @@ class FeatureContext extends BehatContext
      */
     public function iRequest($arg1)
     {
-        $this->response = $this->client->$arg1($this->parameters);
+        $this->response = $this->client->$arg1($this->request_parameters);
+        
     }
     
     /**
@@ -61,14 +68,14 @@ class FeatureContext extends BehatContext
     public function theResponseHasKey($key)
     {
         $data = $this->response;
-
+        
         if (!empty($data)) {
-            if (!isset($data[$key]  )) {
+            if (!isset($data[$key]  ))
                 throw new Exception("Key '".$key."' is not set!\n");
-            }
         } else {
             throw new Exception("Response was not JSON\n");
         }
+        
     }
 
 
