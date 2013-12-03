ebb31729f4d2168a79d284cd68fd077d73c75be5
Simone Fumagalli <simone@ilveinperego.com>
2013-12-03 14:52:28 +0100

    No log system build in

--- src/Bitly/Api/BitlyClient.php
+++ src/Bitly/Api/BitlyClient.php
@@ -4,7 +4,6 @@ namespace Bitly\Api;
 
 use Guzzle\Service\Client;
 use Guzzle\Service\Description\ServiceDescription;
-use Guzzle\Plugin\Log\LogPlugin;
 use Bitly\Api\Plugin\TokenAuthPlugin;
 use Bitly\Api\Subscribers\ArrayAggregatorSubscriber;
 use Bitly\Api\Subscribers\ResponseStandardizationSubscriber;
@@ -24,19 +23,6 @@ class BitlyClient extends Client
         $this->addSubscriber(new ArrayAggregatorSubscriber());
         $this->addSubscriber(new ResponseStandardizationSubscriber());
 
-    }
-    
-    public function enableLogging(\Guzzle\Log\AbstractLogAdapter $adapter = null, $formatter = null) {
-      
-        if (is_null($adapter)) {
-            $adapter = new \Guzzle\Log\MonologLogAdapter(
-              new \Monolog\Logger(new \Monolog\Handler\StreamHandler('php://stdout'))
-            );
-        }
-      
-        $logPlugin = new LogPlugin($adapter, $formatter ? $formatter : \Guzzle\Log\MessageFormatter::DEBUG_FORMAT );
-        $this->addSubscriber($logPlugin);
-        
     }
 
 }
