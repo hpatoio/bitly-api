bcacb4e368ef2b46492f6202b4606a50b6869c8b
Simone Fumagalli <simone@ilveinperego.com>
2013-12-16 11:58:05 +0100

    Fixing SensioLabInsight issue #35-059

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -1,11 +1,7 @@
 <?php
 
-use Behat\Behat\Context\ClosuredContextInterface,
-    Behat\Behat\Context\TranslatedContextInterface,
-    Behat\Behat\Context\BehatContext,
-    Behat\Behat\Exception\PendingException;
-use Behat\Gherkin\Node\PyStringNode,
-    Behat\Gherkin\Node\TableNode;
+use Behat\Behat\Context\BehatContext;
+use Behat\Gherkin\Node\TableNode;
 
 /**
  * Features context.
